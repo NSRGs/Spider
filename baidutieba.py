b2f61df90fdb401f0ef2d6b83fafb027e26bdd2f
Holmes <xiao26@users.noreply.github.com>
2015-10-29 13:16:47 +0800

    Update baidutieba.py

--- baidutieba.py
+++ baidutieba.py
@@ -11,9 +11,7 @@ class Handler(BaseHandler):
     crawl_config = {
             "headers": {
 
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.71 Safari/537.36",
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-            "Accept-Language": "en-US,en;q=0.5",
+
         }
     }
 
